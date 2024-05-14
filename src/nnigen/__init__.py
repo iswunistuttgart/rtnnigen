5c02d80e3943e8b0bf8419d5fd13eccf0ce963d3
Christoph Hinze <christoph.hinze@isw.uni-stuttgart.de>
2024-05-14 09:26:36 +0200

    Cleanup __init__.py nnigen

--- src/nnigen/__init__.py
+++ src/nnigen/__init__.py
@@ -1,3 +1,1 @@
-from nnigen.gen_st import TwinCAT_ST_writer
-from nnigen.parse_model import keras_to_st_parser
-from nnigen.nnigen import nnigen, get_example_usage
+from nnigen.nnigen import nnigen, get_example_usage, overwrite_weights_file
